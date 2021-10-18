e279e67d58eb3c9507aed4d79ad4179cfbfea48e
fabian-nino <falenire@csu.fullerton.edu>
2021-10-17 23:20:33 -0700

    Format

--- Login/Login/LoginViewController.swift
+++ Login/Login/LoginViewController.swift
@@ -8,40 +8,42 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    @IBOutlet weak var username: UITextField!
-    @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var forgotUsernameButton: UIButton!
-    @IBOutlet weak var forgotPasswordButton: UIButton!
-    
+    @IBOutlet var username: UITextField!
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var forgotUsernameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+
     @IBAction func loginAction(_ sender: Any) {
         performSegue(withIdentifier: "loginSegue", sender: sender)
     }
+
     @IBAction func forgotUsernameAction(_ sender: Any) {
         performSegue(withIdentifier: "loginSegue", sender: sender)
     }
+
     @IBAction func fogotPasswordAction(_ sender: Any) {
         performSegue(withIdentifier: "loginSegue", sender: sender)
     }
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+     }
+     */
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let sender = sender as? UIButton else {return}
-        
+        guard let sender = sender as? UIButton else { return }
+
         if sender == forgotPasswordButton {
             segue.destination.navigationItem.title = "Forgot Password"
         } else if sender == forgotUsernameButton {
@@ -50,5 +52,4 @@ class LoginViewController: UIViewController {
             segue.destination.navigationItem.title = username.text
         }
     }
-
 }
