680a2e639ac721382590a749e01f36bb65ced0b9
fabian-nino <falenire@csu.fullerton.edu>
2021-10-17 23:00:32 -0700

    Step 1

---
+++ Login/Login/LandingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LandingViewController.swift
+//  Login
+//
+//  Created by Fabian Nino on 10/17/21.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
